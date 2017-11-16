9a298ee14d704bf58114a7cc8ef098fc6bb8a505
Mike Romain <mchlrmn@me.com>
2017-11-16 14:46:32 +0100

    Installed @types/express-session and @types/errorhandler and env variables.
    TODO: Dev API.

--- src/server.ts
+++ src/server.ts
@@ -16,32 +16,32 @@ app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
-  express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
+	express.static(path.join(__dirname, "public"), { maxAge: 31557600000 })
 );
 
 app.use("/api", apiController);
 app.get("/", (req, res) => {
-  res.json({
-    name: "JS-Talks Back-End",
-    links: [
-      {
-        rel: "api",
-        href: `http://${process.env.HOSTNAME}:${app.get("port")}/api`
-      }
-    ]
-  });
+	res.json({
+		name: "JS-Talks Back-End",
+		links: [
+			{
+				rel: "api",
+				href: `http://${process.env.HOSTNAME}:${app.get("port")}/api`
+			}
+		]
+	});
 });
 
 /**
  * Start Express server.
  */
 app.listen(app.get("port"), () => {
-  console.log(
-    `\tApp is running at http://${process.env.HOSTNAME}:${app.get(
-      "port"
-    )} in ${app.get("env")} mode`
-  );
-  console.log("\tPress CTRL-C to stop\n");
+	console.log(
+		`\tApp is running at http://${process.env.HOSTNAME}:${app.get(
+			"port"
+		)} in ${app.get("env")} mode`
+	);
+	console.log("\tPress CTRL-C to stop\n");
 });
 
 module.exports = app;
