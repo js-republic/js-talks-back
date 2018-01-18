d46cc0db724ca6c7a9a7152d2c2ca59017815b7e
Thomas Rimbault <thomas.rimbault@js-republic.com>
2018-01-18 10:19:02 +0100

    WIP docker-compose > mysql

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,7 +2,39 @@
 
 import * as async from "async";
 import { Response, Request, NextFunction, Router } from "express";
-const db = require("../db-mocks.json");
+
+import Sequelize from "sequelize"
+
+const sequelize = new Sequelize('js_talks_db', 'root', '', {
+    host: 'localhost',
+    dialect: 'mysql',
+    operatorsAliases: false
+})
+
+sequelize.query("SELECT * FROM users").then(datas => {
+    console.log('Query result', datas)
+})
+
+// const mysql = require('mysql');
+
+// const connection = mysql.createConnection({
+//     // port: 3306,
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'js_talks_db'
+// });
+
+// connection.connect();
+
+// connection.query('SELECT * FROM users', (error, results, fields) => {
+    // if (error) throw error;
+    // console.log('The solution is: ', results);
+// });
+
+// connection.end();
+
+// const db = require("../db-mocks.json");
 
 /**
  * GET /api
@@ -12,16 +44,16 @@ const router = Router();
 
 // HOME
 
-router.get("/", (req: Request, res: Response) => {
-    res.json();
-});
+// router.get("/", (req: Request, res: Response) => {
+//     res.json();
+// });
 
-// TALKS
+// // TALKS
 
-router.get("/talks", (req: Request, res: Response) => {
-    const err = db.talks ? false : true;
-    if (err) res.status(404).send("Talks not found");
-    res.status(200).send(db.talks);
-});
+// router.get("/talks", (req: Request, res: Response) => {
+//     const err = db.talks ? false : true;
+//     if (err) res.status(404).send("Talks not found");
+//     res.status(200).send(db.talks);
+// });
 
 export default router;
