2515793cdafb67ffa5ae1c88b9e74c6ae5e8f6c6
Thomas Rimbault <thomas.rimbault@js-republic.com>
2018-01-22 15:40:20 +0100

    Add routes

--- src/controllers/api.ts
+++ src/controllers/api.ts
@@ -2,8 +2,17 @@
 
 import * as async from "async";
 import { Response, Request, NextFunction, Router } from "express";
+import { initDatabase, select } from "./../database"
+import {
+    findTalks,
+    findTalksById,
+    findLikesByTalkId,
+    findSpeakersByTalkId,
+    addTalk,
+    updateTalk,
+    removeTalks,
+} from "./../repository"
 
-const db = require("../db-mocks.json");
 
 /**
  * GET /api
@@ -12,17 +21,47 @@ const db = require("../db-mocks.json");
 const router = Router();
 
 // HOME
-
 router.get("/", (req: Request, res: Response) => {
-    res.json();
+    res.json("/");
 });
 
 // TALKS
-
 router.get("/talks", (req: Request, res: Response) => {
-    const err = db.talks ? false : true;
-    if (err) res.status(404).send("Talks not found");
-    res.status(200).send(db.talks);
+    findTalks().then(datas => res.status(200).json(datas))
+});
+
+router.get("/talks/:talk_id", (req: Request, res: Response) => {
+    const talk_id = req.param('talk_id')
+    findTalksById(talk_id).then(datas => res.status(200).json(datas))
+});
+
+router.get("/talks/:talk_id/likes", (req: Request, res: Response) => {
+    const talk_id = req.param('talk_id')
+    findLikesByTalkId(talk_id).then(datas => res.status(200).json(datas))
+});
+
+router.get('/talks/:talk_id/speakers', (req: Request, res: Response) => {
+    const talk_id = req.param('talk_id')
+    findSpeakersByTalkId(talk_id).then(datas => res.status(200).json(datas))
+});
+
+// Add talk
+router.post("/talks", (req: Request, res: Response) => {
+    const talk = req.body
+    addTalk(talk).then(talkId => res.status(200).json({talk_id: talkId}))
+});
+
+// Update talk
+router.put("/talks/:talk_id", (req: Request, res: Repsonse) => {
+    const talk_id = req.param('talk_id')
+    const talk = req.body
+    updateTalk(talk_id, talk).then(datas => res.status(200).json(datas))
+});
+
+// Delete talk
+router.delete("/talks/:talk_id", (req: Request, res: Response) => {
+    const talk_id = req.param('talk_id')
+    removeTalks(talk_id).then(result => res.status(200))
 });
 
 export default router;
